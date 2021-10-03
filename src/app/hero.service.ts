b8859d6bfafca9c0b029cb3a6e8639508a09e12e
Nolwenn Merlet <nolwenn.merlet@gmail.com>
2021-10-02 21:34:52 -0400

    Tuto en cours, fin journee du 2 oct 2021

---
+++ src/app/hero.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  
+  constructor(private messageService: MessageService) { }
+  
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES); 
+    this.messageService.add('HeroService: fetched heroes')
+    return heroes;
+  }
+
+  /** GET heroes from the server */
+  /*
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+  }
+  */
+
+  
+ 
+}
+
